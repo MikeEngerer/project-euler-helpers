694a3373d086605c9a4b0d52cac1ef980cf2f081
MikeEngerer <michael.engerer@hotmail.com>
2019-05-09 17:25:31 -0400

    now returns false for negative nums

--- src/primes.js
+++ src/primes.js
@@ -1,6 +1,6 @@
 const isPrime = (num) => {
   if (num === 2) return true
-  if (num % 2 === 0 || num === 1) return false
+  if (num % 2 === 0 || num === 1 || num < 0) return false
   for (let i = 3; i < Math.ceil(Math.sqrt(num)) + 1; i += 2) {
     if (num % i === 0) {
       return false
